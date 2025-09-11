c9458ee975f5d11be65fe22c93123510d4a10b50
masroof-maindak <mujtaba.asim.amin@gmail.com>
2025-09-11 17:05:42 +0500

    feat: argparsing + cmake

--- main.cpp
+++ main.cpp
@@ -1,11 +1,37 @@
 #include <print>
 
+#include <argparse/argparse.hpp>
 #include <opencv2/opencv.hpp>
 
 int main(int argc, char *argv[]) {
-	if (argc != 2) {
-		std::println(stderr, "Usage: {} <input-img-path>", argv[0]);
-		return 1;
+	argparse::ArgumentParser prog("chng");
+
+	prog.add_argument("-o", "--output")
+		.required()
+		.help("specify the output dir.");
+
+	prog.add_argument("-i", "--input")
+		.required()
+		.help("specify the input dir.");
+
+	prog.add_argument("-ie", "--input-ext")
+		.help("specify the input frames' extension (png, jpg, jpeg).")
+		.default_value("png");
+
+	prog.add_argument("-oe", "--output-ext")
+		.help("specify the output frames' extension (png, jpg).")
+		.default_value("png");
+
+	prog.add_argument("-vf", "--video-format")
+		.help("specify the output video's extension (mp4, avi).")
+		.default_value("mp4");
+
+	try {
+		prog.parse_args(argc, argv);
+	} catch (const std::exception &err) {
+		std::cerr << err.what() << std::endl;
+		std::cerr << prog;
+		std::exit(1);
 	}
 
 	// Load image
