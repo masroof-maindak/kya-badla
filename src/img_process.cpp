9b7dca7772f321d9979261ed4f4df75ae55f6fbb
masroof-maindak <mujtaba.asim.amin@gmail.com>
2025-09-13 17:44:21 +0500

    chore: sensible renames

--- src/img_process.cpp
+++ src/img_process.cpp
@@ -14,22 +14,22 @@ std::expected<std::vector<cv::Mat>, std::string> bgr_video_to_grayscale(const st
         if (frame.empty() || frame.type() != CV_8UC3)
             return std::unexpected("[GRAYSCALE] Invalid type/empty frame.");
 
-        cv::Mat tmp{};
-        tmp.create(frame.rows, frame.cols, CV_8UC1);
+        cv::Mat gray_frame{};
+        gray_frame.create(frame.rows, frame.cols, CV_8UC1);
 
         for (int y = 0; y < frame.rows; y++) {
-            const cv::Vec3b *src = frame.ptr<cv::Vec3b>(y);
-            std::uint8_t *dst    = tmp.ptr<std::uint8_t>(y);
+            const cv::Vec3b *frame_row = frame.ptr<cv::Vec3b>(y);
+            std::uint8_t *gray_row     = gray_frame.ptr<std::uint8_t>(y);
 
             for (int x = 0; x < frame.cols; x++) {
-                const cv::Vec3b &px = src[x];
+                const cv::Vec3b &px = frame_row[x];
                 auto mm             = std::minmax({px[0], px[1], px[2]});
                 std::uint8_t mean   = (mm.first + mm.second) / 2;
-                dst[x]              = mean;
+                gray_row[x]         = mean;
             }
         }
 
-        video_gray.emplace_back(tmp);
+        video_gray.emplace_back(gray_frame);
     }
 
     return video_gray;
