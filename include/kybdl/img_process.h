f67814708e1fc9b1cd266b7f66a8b1b420d764d7
masroof-maindak <mujtaba.asim.amin@gmail.com>
2025-09-13 17:51:04 +0500

    feat: doxygen-style comments for important fns

--- include/kybdl/img_process.h
+++ include/kybdl/img_process.h
@@ -7,6 +7,10 @@
 #include <string>
 #include <vector>
 
+/**
+ * @param video Input video as a vector of matrices of type CV_8UC3 (BGR uint8_t).
+ * @return A vector of matrices of type CV_8UC1 (grayscale uint8_t), or an error string.
+ */
 std::expected<std::vector<cv::Mat>, std::string> bgr_video_to_grayscale(const std::vector<cv::Mat> &video);
 
 #endif // IMG_PROCESS_H
--- include/kybdl/stats.h
+++ include/kybdl/stats.h
@@ -8,11 +8,29 @@
 #include <string>
 #include <vector>
 
+/**
+ * @param video Input grayscale video as a vector of matrices of type CV_8UC1 (uint8_t).
+ * @param frame_count Optional number of first N frames to use.
+ * @return A grayscale HxW matrix of type CV_32FC1 (float) representing the mean frame, or an error string.
+ */
 std::expected<cv::Mat, std::string> compute_mean(const std::vector<cv::Mat> &video, std::optional<int> frame_count);
 
+/**
+ * @param video Input grayscale video as a vector of matrices of type CV_8UC1 (uint8_t).
+ * @param mean_frame Mean frame as a grayscale HxW matrix of type CV_32FC1 (float).
+ * @param frame_count Optional number of first N frames to use.
+ * @return A grayscale HxW matrix of type CV_32FC1 (float) representing the variance frame, or an error string.
+ */
 std::expected<cv::Mat, std::string> compute_variance(const std::vector<cv::Mat> &video, const cv::Mat &mean_frame,
                                                      std::optional<int> frame_count);
 
+/**
+ * @param video Input grayscale video as a vector of matrices of type CV_8UC1 (uint8_t).
+ * @param mean_frame Mean frame as a grayscale HxW matrix of type CV_32FC1 (float).
+ * @param variance_frame Variance frame as a grayscale HxW matrix of type CV_32FC1 (float).
+ * @param threshold Threshold value for Mahalanobis distance.
+ * @return A vector of matrices of type CV_8UC1 (binary uint8_t) representing the change masks, or an error string.
+ */
 std::expected<std::vector<cv::Mat>, std::string> compute_mask(const std::vector<cv::Mat> &video,
                                                               const cv::Mat &mean_frame, const cv::Mat &variance_frame,
                                                               int threshold);
