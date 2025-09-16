e9d913ffd98b198d8cc2ec11278ab8a994f47e22
masroof-maindak <mujtaba.asim.amin@gmail.com>
2025-09-16 12:27:58 +0500

    docs: fix matrix type

--- include/kybdl/img_process.h
+++ include/kybdl/img_process.h
@@ -18,7 +18,7 @@ std::expected<Video, std::string> bgr_video_to_grayscale(const Video &video);
  * @param video Input video as a vector of matrices of type CV_8UC3 (BGR uint8_t).
  * @param masks Input binarily-thresholded video of masks as a vector of matrices of type CV_8UC1 (uint8_t).
  * @param mean_frame Mean frame as a grayscale HxW matrix of type CV_32FC1 (float).
- * @return A vector of matrices of type CV_8UC1 (grayscale uint8_t), or an error string.
+ * @return A vector of matrices of type CV_8UC3 (BGR uint8_t), or an error string.
  */
 std::expected<Video, std::string> alpha_blend(const Video &video, const Video &masks, const cv::Mat &mean_frame);
 
